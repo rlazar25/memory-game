19297390760677153c5e00ba393d41d83443bf98
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-05-21 16:30:23 +0200

    Logic for flipped cards

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
 let container = document.querySelector('.container');
 createGrid();
-let cards =document.querySelectorAll('.card')
+let cards =document.querySelectorAll('.card');
 cards.forEach(card => card.addEventListener('click', flip));
+let flipped = [];
 
 function flip(e){
-    alert(1)
+    flipped.push(this);
+    this.classList.add('active');
+    if (flipped.length === 2){
+        checkCards();
+    }
 }
+
+function checkCards(){
+    let back1 = flipped[0].querySelector('.back');
+    let back2 = flipped[1].querySelector('.back');
+
+    console.log('flipped')
+    if(back1.innerHTML === back2.innerHTML){
+
+    } else {
+        setTimeout(()=>{
+            flipped.forEach(card => card.classList.remove('active'));
+            flipped = []
+        },700)
+    }
+}
+
 function createGrid(){
     let text = '';
     for (let i = 0; i < 4; i++) {
+        let rand = Math.floor(Math.random()*icons.length)
         text +=`
         <div class="card">
-            <div class="back"></div>
-            <div class="front">${i}</div>
+            <div class="back">${icons[rand]}</div>
+            <div class="front"></div>
         </div>
         `.trim();
+        icons.splice(rand, 1)
     }
     container.innerHTML = text
 }
